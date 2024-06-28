d85f470b62e13e2fd783f4abb1348e56ee29c0f3
CPearl0 <1269596929@qq.com>
2024-06-28 12:52:45 +0800

    chocolate recipes

--- kubejs/server_scripts/Create Confectionery/chocolate.js
+++ kubejs/server_scripts/Create Confectionery/chocolate.js
@@ -9,8 +9,8 @@ ServerEvents.recipes(e => {
             "minecraft:sugar"
         ]
     )
-    .heated()
-    .id("create:mixing/chocolate")
+        .heated()
+        .id("create:mixing/chocolate")
     // 黑巧
     e.recipes.create.mixing(
         Fluid.of("create_confectionery:black_chocolate", 250),
@@ -20,8 +20,33 @@ ServerEvents.recipes(e => {
             Fluid.of("minecraft:milk", 250)
         ]
     )
-    .heated()
-    .id("create_confectionery:black_chocolate_recipe")
+        .heated()
+        .id("create_confectionery:black_chocolate_recipe")
+    e.recipes.create.filling(
+        'createdelightcore:black_chocolate_mold_filled',
+        [
+            Fluid.of("create_confectionery:black_chocolate", 250),
+            'ratatouille:chocolate_mold'
+        ]
+    )
+        .id("create:filling/black_chocolate_mold_filled")
+    e.custom({
+        "type": "ratatouille:demolding",
+        "ingredients": [
+            {
+                "item": "createdelightcore:black_chocolate_mold_solid"
+            }
+        ],
+        "results": [
+            {
+                "item": 'create_confectionery:bar_of_black_chocolate'
+            },
+            {
+                "item": "ratatouille:chocolate_mold"
+            }
+        ]
+    })
+        .id("create_confectionery:bar_of_black_chocolate_recipe")
     // 白巧
     e.recipes.create.mixing(
         Fluid.of("create_confectionery:white_chocolate", 250),
@@ -31,8 +56,33 @@ ServerEvents.recipes(e => {
             "minecraft:sugar"
         ]
     )
-    .heated()
-    .id("create_confectionery:white_chocolate_recipe")
+        .heated()
+        .id("create_confectionery:white_chocolate_recipe")
+    e.recipes.create.filling(
+        'createdelightcore:white_chocolate_mold_filled',
+        [
+            Fluid.of("create_confectionery:white_chocolate", 250),
+            'ratatouille:chocolate_mold'
+        ]
+    )
+        .id("create:filling/white_chocolate_mold_filled")
+    e.custom({
+        "type": "ratatouille:demolding",
+        "ingredients": [
+            {
+                "item": "createdelightcore:white_chocolate_mold_solid"
+            }
+        ],
+        "results": [
+            {
+                "item": 'create_confectionery:bar_of_white_chocolate'
+            },
+            {
+                "item": "ratatouille:chocolate_mold"
+            }
+        ]
+    })
+        .id("create_confectionery:bar_of_white_chocolate_recipe")
     // 红宝石巧克力
     e.recipes.create.mixing(
         [
@@ -46,8 +96,33 @@ ServerEvents.recipes(e => {
             "minecraft:sugar"
         ]
     )
-    .heated()
-    .id("create_confectionery:ruby_chocolate_recipe")
+        .heated()
+        .id("create_confectionery:ruby_chocolate_recipe")
+    e.recipes.create.filling(
+        'createdelightcore:ruby_chocolate_mold_filled',
+        [
+            Fluid.of("create_confectionery:ruby_chocolate", 250),
+            'ratatouille:chocolate_mold'
+        ]
+    )
+        .id("create:filling/ruby_chocolate_mold_filled")
+    e.custom({
+        "type": "ratatouille:demolding",
+        "ingredients": [
+            {
+                "item": "createdelightcore:ruby_chocolate_mold_solid"
+            }
+        ],
+        "results": [
+            {
+                "item": 'create_confectionery:bar_of_ruby_chocolate'
+            },
+            {
+                "item": "ratatouille:chocolate_mold"
+            }
+        ]
+    })
+        .id("create_confectionery:bar_of_ruby_chocolate_recipe")
     // 热巧克力
     e.recipes.create.mixing(
         Fluid.of("create_confectionery:hot_chocolate", 250),
@@ -57,6 +132,6 @@ ServerEvents.recipes(e => {
             "create:bar_of_chocolate"
         ]
     )
-    .heated()
-    .id("create_confectionery:hot_chocolate_recipe")
+        .heated()
+        .id("create_confectionery:hot_chocolate_recipe")
 })
